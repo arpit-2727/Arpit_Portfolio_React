15ebab8886003673d580bbe0f4d58ce8b8949c92
arpit-2727 <arpit1111k@gmail.com>
2024-03-24 11:59:20 +0530

    data

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/arpit-2727",
-  resume: "https://drive.google.com/file/d/19V1XLQENuVNjotLj9_9tsq7u4OfVtcJA/view?usp=drive_link",
+  resume: "https://drive.google.com/file/d/1fYpbj41AGalAvzz7qPi2CQsxBikYtshP/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/arpit_27p/",
   insta: "https://www.instagram.com/arpit_27p/",
 };
@@ -174,6 +174,7 @@ export const projects = [
     tags: ["HTML", "CSS"],
     category: "web app",
     github: "https://github.com/arpit-2727/Basic-Shopping-Website",
+    webapp : "https://shoppping-ui-arpit.netlify.app"
   },
   {
     id: 0,
