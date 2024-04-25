1ca2fb6f9f1b646871f8a5d74db84a483c285d51
arpit-2727 <arpit1111k@gmail.com>
2024-04-26 01:26:07 +0530

    data

--- src/data/constants.js
+++ src/data/constants.js
@@ -140,9 +140,8 @@ export const education = [
     img: "https://images.collegedunia.com/public/college_data/images/campusimage/1478067632CAM2.JPG",
     school: "Institute of Technology  And Management Gida Gorakhpur",
     date: "DEC 2020 - JUNE 2024",
-    grade: "7.4 CGPA AGGREGATE",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    grade: "8.1 CGPA AGGREGATE",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at ITM GIDA GORAKHPUR,. I have completed 7 semesters and have a CGPA of 8.1. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others."
   },
   {
     id: 1,
